faa6d35fe08f9de22aca98c409a6b999e75dfff1
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-06 20:26:01 +0000

    Add cell alive/dead states

---
+++ lib/cell.rb
@@ -0,0 +1,11 @@
+class Cell
+
+  def initialize(alive: false)
+    @alive = alive
+  end
+
+  def alive?
+    @alive
+  end
+
+end
---
+++ spec/cell_spec.rb
@@ -0,0 +1,16 @@
+require 'cell'
+require 'world'
+
+describe Cell do
+  it 'is dead by default' do
+    expect(Cell.new().alive?).to be false
+  end
+
+  it 'can be created alive' do
+    expect(Cell.new(alive: true).alive?).to be true
+  end
+
+  it 'can be created dead' do
+    expect(Cell.new(alive: false).alive?).to be false
+  end
+end
