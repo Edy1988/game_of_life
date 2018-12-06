9b03b9e87730774e599718d5c5673553f5dfaea8
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-06 20:27:05 +0000

    Replace World initializer params with named keywords

--- lib/world.rb
+++ lib/world.rb
@@ -1,12 +1,15 @@
+require_relative 'cell'
+
 class World
 
   attr_reader :rows, :columns, :grid
 
-  def initialize(rows, columns)
+  def initialize(rows:, columns:)
     @rows = rows
     @columns = columns
     @grid = Array.new(rows) do |row|
       Array.new(columns) do |columns|
+        Cell.new
       end
     end
   end
