31000b8060e7d7fdc136048a64448ca04e0f0e08
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-06 21:38:25 +0000

    Proved the World starts with all cells dead

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -2,14 +2,24 @@ require 'world'
 require 'cell'
 
 describe World do
-  
+
   it 'builds a grid (row x colum)' do
     world = World.new(rows: 10, columns: 5)
 
     expect(world.grid.length).to eq 10
 
-    world.grid.each do |column|
-      expect(column.length).to eq 5
+    world.grid.each do |columns|
+      expect(columns.length).to eq 5
+    end
+  end
+
+  it 'starts with dead cells' do
+    world = World.new(rows: 10, columns: 5)
+
+    world.grid.each do |columns|
+      columns.each do |cell|
+        expect(cell.alive?).to be false
+      end
     end
   end
 
